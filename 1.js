65f785bfe747d22ccbeb9d52505a15abdaaec524
liufei1776 <liufei1776@163.com>
2024-06-03 22:44:10 +0800

    2.js commit 1

--- 1.js
+++ 1.js
@@ -2,3 +2,5 @@ console.log('1.js -- change -- 1');
 console.log('1.js -- change -- 2');
 console.log('1.js -- change -- 3');
 console.log('1.js -- change -- 4');
+console.log('1.js -- change -- A');
+console.log('1.js -- change -- B');
---
+++ 2.js
@@ -0,0 +1,2 @@
+console.log('2.js -- change -- 1');
+console.log('2.js -- change -- 2');
